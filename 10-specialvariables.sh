0f61494ae89668a8468e4b6a2a6ec6cf3a64f9b9
phanindra Reddy <phani.vaj16@gmail.com>
2024-04-16 15:14:00 +0530

    shell scipting practice

--- 10-specialvariables.sh
+++ 10-specialvariables.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-echo "all the varaibles are::" $@
+echo "all the parameters are::" $@
 echo "Username is:" $uid
 echo "process id is:" $$
+echo "Number of input parameters::" $#
+echo "all the inputs in one line::" $*
