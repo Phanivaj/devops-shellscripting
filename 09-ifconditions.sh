ee16899622ae36dbf226cf04f99d332926cb4388
phanindra Reddy <phani.vaj16@gmail.com>
2024-04-16 14:50:07 +0530

    shell scipting practice

--- 09-ifconditions.sh
+++ 09-ifconditions.sh
@@ -6,7 +6,7 @@ read finput
 echo "Enter second input::"
 read sinput
 
-if[$finput -gt $sinput];then
+if [$finput -gt $sinput]; then
 echo "First Input is greater than second input"
 elif [$finput -lt $sinput]; then
 echo  "Second Input is greater than first input"
